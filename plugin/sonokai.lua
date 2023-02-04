83c61cbb077d77135d820ae6da663bc5ea6c455a
eliaskountouris <eliaskountouris9@gmail.com>
2023-02-04 12:46:20 -0500

    Packer + Colorscheme

---
+++ plugin/sonokai.lua
@@ -0,0 +1,7 @@
+vim.g.sonokai_style = 'andromeda'
+vim.g.sonokai_better_preformance = 1
+vim.g.sonokai_dim_inactive_windows = 1
+vim.g.sonokai_diagnostic_line_highlight = 1
+vim.g.sonokai_diagnostic_virtual_text = 'colored'
+
+vim.cmd[[colorscheme sonokai]]

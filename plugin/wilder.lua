3ed800bf4433a4e904f69693366a11c8397842eb
eliaskountouris <eliaskountouris9@gmail.com>
2023-02-28 22:25:30 -0500

    Reverted Wilder

--- plugin/wilder.lua
+++ plugin/wilder.lua
@@ -1,21 +1,12 @@
 local wilder = require('wilder')
 wilder.setup({modes = {':'}})
 
-wilder.set_option('renderer', wilder.popupmenu_renderer({
-  -- highlighter applies highlighting to the candidates
+wilder.set_option('renderer', wilder.wildmenu_renderer({
   highlighter = wilder.basic_highlighter(),
+  separator = '   ',
+  left = {' ', wilder.wildmenu_spinner(), ' '},
+  right = {' ', wilder.wildmenu_index()},
 }))
-
-wilder.set_option('renderer', wilder.popupmenu_renderer(
-    wilder.popupmenu_border_theme({
-      highlights = {
-        border = 'Normal', -- highlight to use for the border
-      },
-      -- 'single', 'double', 'rounded' or 'solid'
-      -- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
-      border = 'rounded',
-    })
-))
 
 wilder.set_option('pipeline', {
     wilder.branch(

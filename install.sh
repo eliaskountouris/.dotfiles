4d6519c2edf0621d1288555f1800e4373a9b2625
eliaskountouris <eliaskountouris9@gmail.com>
2023-04-05 23:22:41 -0400

    Fixed installer

--- install.sh
+++ install.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
+set -e
+
+if [ $# -eq 0 ]; then
+	echo "Missing path to home!"
+	exit -1
+fi
+
 # zsh install
-cmd="sh zsh/install_ZSH.sh"
+cmd="sh zsh/install_ZSH.sh $1"
 echo $cmd
 $cmd
 echo "ZSH Installed!"
 
 # Link nvim
-cmd="ln -sf $PWD/nvim $HOME/.config/nvim"
+cmd="ln -sf $PWD/nvim $1/.config/nvim"
 echo $cmd
 $cmd
 echo "Linked nvim!"
@@ -15,7 +22,7 @@ echo "Linked nvim!"
 for file in *;
 do
 	if [[ -f $file ]]; then
-		cmd="ln -sf $PWD/$file $HOME/.$file"
+		cmd="ln -sf $PWD/$file $1/.$file"
 		echo $cmd
 		$cmd
 		echo "Linked $file!"
--- zsh/install_ZSH.sh
+++ zsh/install_ZSH.sh
@@ -1,25 +1,34 @@
 #!/bin/sh
 
-export ZSH=$HOME/.oh-my-zsh
+set -e
+
+if [ $# -eq 0 ]; then
+	echo "Missing path to home!"
+	exit -1
+fi
+
+export ZSH=$1/.oh-my-zsh
 
 # install ZSH
 pacman -S zsh
 echo "ZSH Installed!"
 
 # install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if [ ! -d "$1/.oh-my-zsh" ]; then
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
 
 # Set as default shell
 chsh -s $(which zsh)
 echo "$SHELL is default shell!"
 
 # Link zshrc
-cmd="ln -sf $PWD/zsh/zshrc $HOME/.zshrc"
+cmd="ln -sf $PWD/zsh/zshrc $1/.zshrc"
 echo $cmd
 $cmd
 echo "Linked zshrc!" 
 
-cmd="ln -sf $PWD/zsh/aliases $HOME/.aliases"
+cmd="ln -sf $PWD/zsh/aliases $1/.aliases"
 echo $cmd
 $cmd
 echo "Linked aliases!" 

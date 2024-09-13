569ff419f9d4e4e730e74d76371e604018d93741
Elias K <eliaskountouris9@gmail.com>
2024-09-12 20:50:35 -0400

    gitblame off by default

--- nvim/lua/configs/git-blame.lua
+++ nvim/lua/configs/git-blame.lua
@@ -1,4 +1,5 @@
 vim.g.gitblame_display_virtual_text = 1
 vim.g.gitblame_message_when_not_committed = 'not commited'
 vim.g.gitblame_message_template = '<summary> - <author> - <date>'
+vim.g.gitblame_enabled = false
 
